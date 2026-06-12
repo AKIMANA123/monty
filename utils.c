dc9465cbc7bf5c80db9d97c10697ea063f4643cb
AKIMANA123 <akimana123@github.com>
2026-06-12 12:24:47 -0700

    Add utils.c with is_integer function

---
+++ utils.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * is_integer - checks if a string is an integer
+ * @str: string to check
+ *
+ * Return: 1 if integer, 0 otherwise
+ */
+int is_integer(char *str)
+{
+int i = 0;
+
+if (str == NULL || str[0] == '\0')
+return (0);
+
+if (str[0] == '-')
+i = 1;
+
+for (; str[i] != '\0'; i++)
+{
+if (!isdigit(str[i]))
+return (0);
+}
+return (1);
+}

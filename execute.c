3f07344e619c890fdae4bfa977aec68f13cbda5f
AKIMANA123 <axelakimana78@gmail.com>
2026-06-12 11:50:53 +0200

    Refactor execute_opcode for improved readability

--- execute.c
+++ execute.c
@@ -8,37 +8,37 @@
  * @line_number: line number in the file
  */
 void execute_opcode(char *opcode, char *arg, stack_t **stack,
-    unsigned int line_number)
+		    unsigned int line_number)
 {
-if (strcmp(opcode, "push") == 0)
-{
-push(stack, line_number, arg);
-}
-else if (strcmp(opcode, "pall") == 0)
-{
-pall(stack, line_number);
-}
-else if (strcmp(opcode, "pint") == 0)
-{
-pint(stack, line_number);
-}
-else if (strcmp(opcode, "pop") == 0)
-{
-pop(stack, line_number);
-}
-else if (strcmp(opcode, "swap") == 0)
-{
-swap(stack, line_number);
-}
-else if (strcmp(opcode, "add") == 0)
-{
-add(stack, line_number);
-}
-else
-{
-fprintf(stderr, "L%u: unknown instruction %s\n",
-line_number, opcode);
-free_stack(*stack);
-exit(EXIT_FAILURE);
-}
+	if (strcmp(opcode, "push") == 0)
+	{
+		push(stack, line_number, arg);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		pall(stack, line_number);
+	}
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		pint(stack, line_number);
+	}
+	else if (strcmp(opcode, "pop") == 0)
+	{
+		pop(stack, line_number);
+	}
+	else if (strcmp(opcode, "swap") == 0)
+	{
+		swap(stack, line_number);
+	}
+	else if (strcmp(opcode, "add") == 0)
+	{
+		add(stack, line_number);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n",
+			line_number, opcode);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
 }
